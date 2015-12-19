4c328c2b6f79a1482e48648a9f01f9a9f561fe27
trodi <troy.t.mckinnon@gmail.com>
2015-12-18 21:53:42 -0600

    impl random and daily

--- js/app.js
+++ js/app.js
@@ -3,25 +3,39 @@ angular.module("dirtyThirty", [])
         "use strict";
 
         var ctrl = this;
-        ctrl.reasons = ReasonService.reasons;
-        // TODO: make daily
+
+        /* set reasons to reason for today */
         ctrl.setDaily = function() {
-            ctrl.reasons = [ReasonService.reasons[0]];
+            var todayReason = ReasonService.reasons[today];
+            if (!todayReason) { // just in case we miss a date
+                todayReason = ReasonService.reasons[0];
+            }
+            ctrl.reasons = [todayReason];
         }
-        // TODO: make random
+        /* set reasons to a ransom reason */
         ctrl.setRandom = function() {
-            ctrl.reasons = [ReasonService.reasons[1]];
+            var index = Math.round(Math.random() * ReasonService.max);
+            var randomReason = ReasonService.reasons[index];
+            if (!randomReason) { // just in case we miss a date
+                randomReason = ReasonService.reasons[0];
+            }
+            ctrl.reasons = [randomReason];
         }
+        /* set reasons to all reasons */
         ctrl.setAll = function() {
             ctrl.reasons = ReasonService.reasons;
         }
+        var date = new Date();
+        var today = date.getDate();
+        ctrl.setDaily();
     })
     .service("ReasonService", function() {
-        this.reasons = [
+        this.reasons = [ // should be 31 long
             "example 1",
             "example 2",
             "example 3",
             "example 4",
             "example 5"
         ];
+        this.max = this.reasons.length;
     });
