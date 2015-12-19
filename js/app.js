8f817a85bf04248ed0557af141c71bacdc5f5a5f
trodi <troy.t.mckinnon@gmail.com>
2015-12-18 19:28:03 -0600

    stubbed out support for filters

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 angular.module("dirtyThirty", [])
-    .controller("ReasonCtrl", function($scope) {
+    .controller("ReasonCtrl", function(ReasonService) {
         "use strict";
-        $scope.reasons = [];
-        $scope.reasons.push("example 1");
-        $scope.reasons.push("example 2");
-        $scope.reasons.push("example 3");
-        $scope.reasons.push("example 4");
-        $scope.reasons.push("example 5");
+
+        var ctrl = this;
+        ctrl.reasons = ReasonService.reasons;
+        // TODO: make daily
+        ctrl.setDaily = function() {
+            ctrl.reasons = [ReasonService.reasons[0]];
+        }
+        // TODO: make random
+        ctrl.setRandom = function() {
+            ctrl.reasons = [ReasonService.reasons[1]];
+        }
+        ctrl.setAll = function() {
+            ctrl.reasons = ReasonService.reasons;
+        }
+    })
+    .service("ReasonService", function() {
+        this.reasons = [
+            "example 1",
+            "example 2",
+            "example 3",
+            "example 4",
+            "example 5"
+        ];
     });
